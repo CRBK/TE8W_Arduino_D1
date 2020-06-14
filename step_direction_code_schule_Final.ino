071ad2351b34d677a01948825bf26d4b5b7f9abd
Sniggi-star <60892546+Sniggi-star@users.noreply.github.com>
2020-06-14 21:08:12 +0200

    Update step_direction_code_schule_Final.ino

--- step_direction_code_schule_Final.ino
+++ step_direction_code_schule_Final.ino
@@ -9,10 +9,10 @@ Author: Niklas Schröder and Marco Thielen
 
 // User setup
 bool refdirection = true;	//Reference direction, true = right, false = left
-int lifting = 10;			//Lifting in mm
+int lifting = 10;		//Lifting in mm
 const float SPR = 200;		//Motor steps per rotation (360°)
 const int feed = 10;		//Feed of axis (mm/rotation)
-const int gear = 1;			//Gear, 1 = 1:1, 5 = 5:1
+const int gear = 1;		//Gear, 1 = 1:1, 5 = 5:1
 const int acceleration = 1;	//Acceleration (mm/s²)
 const float speed = 10;		//Speed (mm/s)
 const float refspeed = 1;	//Reference speed (mm/s)
@@ -23,23 +23,23 @@ const int step = 2;  		//Output direction
 const int direction = 3; 	//Output step
 const int enable = 4;		//Output enable
 
-const int LS = 5; 			//Input reference switch
+const int LS = 5; 		//Input reference switch
 const int start = 6;		//Input for starting the movement 
-const int stop = 7;			//Input for stoping the movement
+const int stop = 7;		//Input for stoping the movement
 
 
 // Systemvariables
-  int statestart;			//Variable to read state of input start
-  int statestop;			//Variable to read state of input stop
-  int stateLS;				//Variable to read state of input LS
-  long SPlift;				//Steps per lift
-  bool run;					//Starts steps to run
+  int statestart;		//Variable to read state of input start
+  int statestop;		//Variable to read state of input stop
+  int stateLS;			//Variable to read state of input LS
+  long SPlift;			//Steps per lift
+  bool run;			//Starts steps to run
   long stepscounted;		//Counted lift steps
-  float calcspeed;			//Calculated pausetime for normal speed
+  float calcspeed;		//Calculated pausetime for normal speed
   float calcrefspeed;		//Calculated reference speed
-  float steptime;			//Time per step/2
-  bool Refdone;				//Is true if reference run is done
-  bool Ref;					//Runs Reverence
+  float steptime;		//Time per step/2
+  bool Refdone;			//Is true if reference run is done
+  bool Ref;			//Runs Reverence
 
 
 void setup() {
@@ -70,7 +70,7 @@ void setup() {
 // Calculation for speed time (calcspeed = (1000000/((speed/feed)*SPR*gear))/2;)
   calcspeed=float(500000/(float(speed/feed*SPR*gear)));
   
-// Calculation for speed time (calcspeed = (1000000/((refspeed/feed)*SPR*gear))/2;)
+// Calculation for speed time (calcrefspeed = (1000000/((refspeed/feed)*SPR*gear))/2;)
   calcrefspeed=float(500000/(float(refspeed/feed*SPR*gear)));
   
   
@@ -141,10 +141,10 @@ void loop() {
 
 void Reference()
 {
-    calcrefspeed = steptime;
-  	digitalWrite(enable,HIGH);
-    run = true;
-  	stepscounted = 0;
+  calcrefspeed = steptime;
+  digitalWrite(enable,HIGH);
+  run = true;
+  stepscounted = 0;
     if(stateLS == LOW)
     {
       	if(refdirection == true)digitalWrite(direction,LOW);
@@ -160,7 +160,7 @@ void Reference()
 
 void steps()
 {
-  		digitalWrite(step,HIGH);
+  	digitalWrite(step,HIGH);
     	delayMicroseconds(steptime);
     	digitalWrite(step,LOW);
     	delayMicroseconds(steptime);
